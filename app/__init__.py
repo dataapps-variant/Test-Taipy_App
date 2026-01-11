0ec124f0e79826edba7ec7683fb340b7707095b9
dataapps-variant <dataapps@variant.net>
2026-01-12 01:18:51 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-# Variant Analytics Dashboard - Taipy Version
-__version__ = "2.0.0"
--- app/pages/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pages package - Individual page modules can be added here if needed
-# Currently, all pages are defined in main.py for simplicity
